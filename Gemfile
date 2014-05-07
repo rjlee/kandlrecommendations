5767e62deb5ce9336bd7a94fd12bab7847b39f8d
Rob Lee <robl+git@rjlee.net>
2014-05-07 21:09:41 +0100

    Adding postgres in for production

--- Gemfile
+++ Gemfile
@@ -1,9 +1,14 @@
 source 'https://rubygems.org'
 ruby "1.9.3"
 gem 'sinatra'
-gem "mysql"
 gem "activerecord"
 gem "sinatra-activerecord"
+gem "rake"
 
-gem "pismo"
-gem "google_scraper"
+group :development do
+  gem "mysql"
+end
+
+group :production do
+  gem "pg"
+end
--- config/environments.rb
+++ config/environments.rb
@@ -1,7 +1,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-	db = URI.parse(ENV['DATABASE_URL'] || 'mysql://localhost/kandlrecommendations')
+	db = URI.parse(ENV['DATABASE_URL']
  
 	ActiveRecord::Base.establish_connection(
 			:adapter => db.scheme,
