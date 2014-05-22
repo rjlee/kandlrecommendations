6fc257b82d373ee8453807c605abe38dccbc49af
Rob Lee <robl+git@rjlee.net>
2014-05-22 20:28:05 +0100

    Added promo visualisation endpoint and dumps

--- web.rb
+++ web.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
+require 'nokogiri'
 require 'json'
+require 'open-uri'
 require 'pp'
 require 'sinatra/activerecord'
 require './config/environments'
@@ -27,4 +29,68 @@ get '/similar' do
   end
   
   JSON.pretty_generate(similar)
+end
+
+get '/guides/:id' do
+  id = params[:id]
+  url = "http://www.bbc.co.uk/guides/#{id}"
+  source = open(url, &:read)
+
+  promo = ''
+  count = 0
+  document = Document.find_by_url(url)
+  iwonder1 = ''
+  iwonder2 = ''
+  gel1 = ''
+  gel2 = ''
+  gel3 = ''
+  gel4 = ''
+  document.similarities.each do |similarity|
+    count+=1;
+    next if count == 1
+
+    if similarity.similar_document.url =~ /\/guides/
+      if iwonder1.empty?
+        p =erb :_promo_iwonder, :locals => {:id => 1, :link => similarity.similar_document.url, :image => similarity.similar_document.image, :title => similarity.similar_document.title} 
+        iwonder1 = p
+        next
+      end
+      if iwonder2.empty?
+        p =erb :_promo_iwonder, :locals => {:id => 2, :link => similarity.similar_document.url, :image => similarity.similar_document.image, :title => similarity.similar_document.title} 
+        iwonder2 = p
+        next
+      end
+    else 
+      if gel1.empty?
+        p =erb :_promo_gel, :locals => {:id => 3, :link => similarity.similar_document.url, :image => similarity.similar_document.image, :title => similarity.similar_document.title} 
+        gel1 = p
+        next
+      end
+       if gel2.empty?
+        p =erb :_promo_gel, :locals => {:id => 4, :link => similarity.similar_document.url, :image => similarity.similar_document.image, :title => similarity.similar_document.title} 
+        gel2 = p
+        next
+      end
+       if gel3.empty?
+        p =erb :_promo_gel, :locals => {:id => 5, :link => similarity.similar_document.url, :image => similarity.similar_document.image, :title => similarity.similar_document.title} 
+        gel3 = p
+        next
+      end
+       if gel4.empty?
+        p =erb :_promo_gel, :locals => {:id => 6, :link => similarity.similar_document.url, :image => similarity.similar_document.image, :title => similarity.similar_document.title} 
+        gel4 = p
+        next
+      end
+    end
+  end
+
+  promo = iwonder1+iwonder2+gel1+gel2+gel3+gel4
+  doc = Nokogiri::HTML(source)
+  doc.search("//div[contains(@class, 'promos')]").each do |node|
+    node.children.remove
+    node.inner_html=promo
+  end
+
+
+  doc.to_html
 end
